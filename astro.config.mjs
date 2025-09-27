091804ca3fd2351922ae75aaa1bb027740649fd9
Matteo Salvestrini <matteo.salvestrini@icloud.com>
2025-09-27 12:20:01 +0200

    Add markdown configuration and dependencies for table of contents and accessible emojis
    Signed-off-by: Matteo Salvestrini <matteo.salvestrini@icloud.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+// import remarkToc from 'remark-toc';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {    
+    syntaxHighlight: 'prism'
+  },
+  // mardown: {
+  //   remarkPlugins: [[remarkToc, {heading: 'toc', maxDepth: 3}]],
+  //   rehypePlugins: []
+  // },
   vite: {
     plugins: [tailwindcss()]
   }
